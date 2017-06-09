40369d01725e83bfa43b8e3514ec1a9761656732
Bill Kindig <wkindig88@gmail.com>
2017-06-08 22:20:48 -0500

    Second Commit

--- app.js
+++ app.js
@@ -17,13 +17,14 @@ app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(stylus.middleware(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'public')));
-
+app.use(express.static('public'))
 app.use('/', index);
 app.use('/users', users);
 
